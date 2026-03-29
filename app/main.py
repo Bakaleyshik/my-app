ca47338f10b19022372988408dd51da3dc74de74
Bakaleyshik <kris.crazy2015@gmail.com>
2026-03-29 15:44:49 +0500

    feat: add configmap and secret

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello from FastAPI in Kubernetes! 🚀"}
+    return {
+        "message": "Hello from FastAPI in Kubernetes! 🚀",
+        "env": os.getenv("APP_ENV", "unknown"),
+    }
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
